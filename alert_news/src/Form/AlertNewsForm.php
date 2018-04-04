03879ae787ebfd86b4c878c01acf72c700dd968c
Robert Kasza <kaszarobi.27@gmail.com>
2018-04-04 12:50:27 +0200

    db manipulation added

--- alert_news/src/Form/AlertNewsForm.php
+++ alert_news/src/Form/AlertNewsForm.php
@@ -4,7 +4,6 @@ namespace Drupal\alert_news\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\user\Entity\User;
 
 class AlertNewsForm extends FormBase
 {
@@ -21,7 +20,8 @@ class AlertNewsForm extends FormBase
      */
     private function getHirTipusokFromDatabase() {
         //SELECT name FROM `taxonomy_term_field_data` WHERE vid = 'hirtipusok'
-        $result = \Drupal::database()->select('taxonomy_term_field_data', 't')
+        $result = \Drupal::database()
+            ->select('taxonomy_term_field_data', 't')
             ->fields('t', ['tid'])
             ->fields('t', ['name'])
             ->condition('vid', 'hirtipusok', '=')
@@ -33,7 +33,8 @@ class AlertNewsForm extends FormBase
 
     private function getSubscribedNewsTypeListForUser($id, array &$newsTypes) {
         //SELECT * FROM `module_alert_news` WHERE uid = 1
-        $result = \Drupal::database()->select('module_alert_news', 'an')
+        $result = \Drupal::database()
+            ->select('module_alert_news', 'an')
             ->fields('an', ['uid'])
             ->fields('an', ['tid'])
             ->condition('uid', $id, '=')
@@ -54,14 +55,16 @@ class AlertNewsForm extends FormBase
         return false;
     }
 
-
+    private function getCurrentUserId() {
+        return \Drupal::currentUser()->id();
+    }
     /**
      * {@inheritdoc}
      */
     public function buildForm(array $form, FormStateInterface $form_state) {
         $hirtipusok = $this->getHirTipusokFromDatabase();
 
-        $userid = \Drupal::currentUser()->id();
+        $userid = $this->getCurrentUserId();
 
         $subscribednews = $this->getSubscribedNewsTypeListForUser($userid, $hirtipusok);
 
@@ -93,6 +96,73 @@ class AlertNewsForm extends FormBase
 
     }
 
+    private function getSelectedUserAndNewsCategoryFromDb($uid, $tid) {
+        $result = \Drupal::database()
+            ->select('module_alert_news', 'an')
+            ->fields('an', ['uid'])
+            ->fields('an', ['tid'])
+            ->condition('uid', $uid, '=')
+            ->condition('tid', $tid, '=')
+            ->execute()
+            ->fetchAll();
+
+        return $result;
+    }
+
+    /**
+     * @param $uid
+     * @param $tid
+     * @return \Drupal\Core\Database\StatementInterface|int|null
+     * @throws \Exception
+     */
+    private function createNewSubscription($uid, $tid) {
+        // INSERT INTO `module_alert_news` (uid, tid) VALUES (1, 4)
+        $result = \Drupal::database()
+            ->insert('module_alert_news')
+            ->fields(
+                array(
+                    'uid' => $uid,
+                    'tid' => $tid,
+                )
+            )
+            ->execute();
+
+        return $result;
+    }
+
+    private function removeSubscriptionFromUser($uid, $tid) {
+        // DELETE FROM `module_alert_news` WHERE uid = 1 AND tid = 4
+        $result = \Drupal::database()
+            ->delete('module_alert_news')
+            ->condition('uid', $uid, "=")
+            ->condition('tid', $tid, "=")
+            ->execute();
+
+        return $result;
+    }
+
+    /**
+     * @param $uid
+     * @param $tid
+     * @param $value
+     * @throws \Exception
+     */
+    private function setValueInDatabase($uid, $tid, $value) {
+        if ($value == "1") {
+            // check if record exists about that
+            if (empty($this->getSelectedUserAndNewsCategoryFromDb($uid, $tid))) {
+                // if there's no record about that subscription, add one to database
+                $result = $this->createNewSubscription($uid, $tid);
+            }
+            // otherwise we don't need to store anything new in db
+
+        } else {
+            // remove the record from database
+            $result = $this->removeSubscriptionFromUser($uid, $tid);
+
+        }
+    }
+
     /**
      * For debugging purposes
      * @param FormStateInterface $form_state
@@ -109,6 +179,19 @@ class AlertNewsForm extends FormBase
      * {@inheritdoc}
      */
     public function submitForm(array &$form, FormStateInterface $form_state) {
+        // display form data for debugging
         $this->displayReturnedForm($form_state);
+
+        // get userid
+        $userid = $this->getCurrentUserId();
+
+        foreach ($form_state->getValues() as $key => $value) {
+            // get tid
+            if (strpos($key, 'alert_news') !== false) {
+                $tid = substr($key, 11);
+                // save in db if necessary
+                $this->setValueInDatabase($userid, $tid, $value);
+            }
+        }
     }
 }
